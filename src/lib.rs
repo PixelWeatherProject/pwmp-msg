347525eed33620ceb72fd8dfa9bc85f865d9793a
br0kenpixel <23280129+br0kenpixel@users.noreply.github.com>
2024-11-30 22:49:11 +0100

    add methods to `Message`

--- src/lib.rs
+++ src/lib.rs
@@ -15,3 +15,59 @@ pub enum Message {
     /// Server responded to a request from a client or vice-versa.
     Response(response::Response),
 }
+
+impl Message {
+    /// Serialize the message into raw bytes.
+    #[must_use]
+    pub fn serialize(self) -> Vec<u8> {
+        bincode::serialize(&self).unwrap()
+    }
+
+    /// Deserialize a message from raw bytes.
+    #[must_use]
+    pub fn deserialize(bytes: &[u8]) -> Option<Self> {
+        bincode::deserialize(bytes).ok()
+    }
+
+    /// Returns a reference to the contained [`Request`].
+    /// If the message contains a [`Response`] instead, `None` is returned.
+    #[must_use]
+    pub const fn request(&self) -> Option<&request::Request> {
+        if let Self::Request(req) = self {
+            Some(req)
+        } else {
+            None
+        }
+    }
+
+    /// Returns a reference to the contained [`Response`].
+    /// If the message contains a [`Request`] instead, `None` is returned.
+    #[must_use]
+    pub const fn response(&self) -> Option<&response::Response> {
+        if let Self::Response(resp) = self {
+            Some(resp)
+        } else {
+            None
+        }
+    }
+
+    /// Similar to [`request()`](Self::request), but consumes the message itself.
+    #[must_use]
+    pub fn as_request(self) -> Option<request::Request> {
+        if let Self::Request(req) = self {
+            Some(req)
+        } else {
+            None
+        }
+    }
+
+    /// Similar to [`response()`](Self::response), but consumes the message itself.
+    #[must_use]
+    pub fn as_response(self) -> Option<response::Response> {
+        if let Self::Response(resp) = self {
+            Some(resp)
+        } else {
+            None
+        }
+    }
+}
