bdb4fb83fb51faeb9a462d430cf9e806cff2298b
br0kenpixel <23280129+br0kenpixel@users.noreply.github.com>
2025-03-18 09:56:25 +0100

    add tests for de/serialization of responses

---
+++ tests/response_deserialization.rs
@@ -0,0 +1,47 @@
+use pwmp_msg::{response::Response, settings::NodeSettings, version::Version, Message};
+
+macro_rules! generate_test {
+    ($test_name: ident, $res: expr) => {
+        #[test]
+        fn $test_name() {
+            let response: Response = $res;
+            let original_message = Message::new_response(response, 1);
+            let serialized = original_message.clone().serialize();
+            let deserialized = Message::deserialize(&serialized).unwrap();
+
+            assert_eq!(original_message, deserialized);
+        }
+    };
+}
+
+generate_test!(can_serialize_pong, Response::Pong);
+
+generate_test!(can_serialize_ok, Response::Ok);
+
+generate_test!(can_serialize_reject, Response::Reject);
+
+generate_test!(can_serialize_fw_up_to_date, Response::FirmwareUpToDate);
+
+generate_test!(
+    can_serialize_update_available,
+    Response::UpdateAvailable(Version::default())
+);
+
+generate_test!(
+    can_serialize_update_part,
+    Response::UpdatePart(b"blob".to_vec().into_boxed_slice())
+);
+
+generate_test!(can_serialize_update_end, Response::UpdateEnd);
+
+generate_test!(can_serialize_empty_settings, Response::Settings(None));
+generate_test!(
+    can_serialize_settings,
+    Response::Settings(Some(NodeSettings {
+        battery_ignore: true,
+        ota: true,
+        sleep_time: 16,
+        sbop: false,
+        mute_notifications: true
+    }))
+);
