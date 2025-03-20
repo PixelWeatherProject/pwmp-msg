eb535ae59fc1e77cdf9d12fddc09b0f2ccc6c6d7
br0kenpixel <23280129+br0kenpixel@users.noreply.github.com>
2025-03-20 08:05:46 +0100

    add docs to `Version`

--- src/version.rs
+++ src/version.rs
@@ -62,24 +62,62 @@ impl Version {
     }
 
     /// Returns the major number from this version.
+    ///
+    /// ```rust
+    ///  use pwmp_msg::version::Version;
+    ///
+    /// let from_string = Version::parse("1.2.6").unwrap();
+    /// let from_ints = Version::new(1, 2, 6);
+    ///
+    /// assert_eq!(from_string.major(), from_ints.major());
+    /// ```
     #[must_use]
     pub const fn major(&self) -> u8 {
         self.major
     }
 
     /// Returns the middle number from this version.
+    ///
+    /// ```rust
+    ///  use pwmp_msg::version::Version;
+    ///
+    /// let from_string = Version::parse("1.2.6").unwrap();
+    /// let from_ints = Version::new(1, 2, 6);
+    ///
+    /// assert_eq!(from_string.middle(), from_ints.middle());
+    /// ```
     #[must_use]
     pub const fn middle(&self) -> u8 {
         self.middle
     }
 
     /// Returns the minor number from this version.
+    ///
+    /// ```rust
+    ///  use pwmp_msg::version::Version;
+    ///
+    /// let from_string = Version::parse("1.2.6").unwrap();
+    /// let from_ints = Version::new(1, 2, 6);
+    ///
+    /// assert_eq!(from_string.minor(), from_ints.minor());
+    /// ```
     #[must_use]
     pub const fn minor(&self) -> u8 {
         self.minor
     }
 
     /// Converts the version to a triple of signed 16-bit integers.
+    ///
+    /// ```rust
+    ///  use pwmp_msg::version::Version;
+    ///
+    /// let version = Version::new(1, 4, 7);
+    /// let (major, middle, minor): (i16, i16, i16) = version.to_signed_triple();
+    ///
+    /// assert_eq!(version.major().try_into(), Ok(major));
+    /// assert_eq!(version.middle().try_into(), Ok(middle));
+    /// assert_eq!(version.minor().try_into(), Ok(minor));
+    /// ```
     #[must_use]
     pub const fn to_signed_triple(&self) -> (i16, i16, i16) {
         (self.major as _, self.middle as _, self.minor as _)
