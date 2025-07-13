6d2fbcab49bf7f436f77bc91ae55d27581dfec58
br0kenpixel <23280129+br0kenpixel@users.noreply.github.com>
2025-07-13 13:40:45 +0200

    delete unused module

--- src/serde/base.rs
+++
@@ -1,9 +0,0 @@
-//! Trants and their implementations for de/serialization.
-
-/// Trait for an object that is serializable.
-pub trait Serializable {
-    /// Serialize this object into a buffer of bytes.
-    fn serialize_into(&self, buffer: &mut Vec<u8>);
-
-    fn deserialize_from(&self, buffer: &mut Vec<u8>);
-}
