c901543dc4092e258c40e4dded48a9f0e475f7f1
br0kenpixel <23280129+br0kenpixel@users.noreply.github.com>
2025-07-20 08:57:06 +0200

    fix incorrect array access breaking deserialization

--- src/serde/mod.rs
+++ src/serde/mod.rs
@@ -1,7 +1,7 @@
 //! A Serialization and deserialization implementation for PWMP.
 
 use super::{request::Request, response::Response, Message, MessageContent};
-use crate::{mac::Mac, serde::utils::next_byte, settings::NodeSettings, MsgId};
+use crate::{mac::Mac, serde::utils::next_byte, settings::NodeSettings};
 use error::Deserialize as DeserializeError;
 
 pub mod consts;
@@ -150,16 +150,16 @@ fn serialize_response(res: Response, buffer: &mut Vec<u8>) {
 }
 
 /// Deserialize a request.
-pub fn deserialize_request(bytes: &[u8]) -> Result<Request, DeserializeError> {
-    let mut bytes = bytes.iter().copied();
-
+pub fn deserialize_request(
+    bytes: &mut impl Iterator<Item = u8>,
+) -> Result<Request, DeserializeError> {
     // get the request type
-    let req_type = utils::next_byte(&mut bytes)?;
+    let req_type = utils::next_byte(bytes)?;
 
     match req_type {
         consts::REQ_KIND_PING => Ok(Request::Ping),
         consts::REQ_KIND_HANDSHAKE => {
-            let octets = utils::next_bytes::<6>(&mut bytes)?;
+            let octets = utils::next_bytes::<6>(bytes)?;
             let mac = Mac::new(
                 octets[0], octets[1], octets[2], octets[3], octets[4], octets[5],
             );
@@ -167,9 +167,9 @@ pub fn deserialize_request(bytes: &[u8]) -> Result<Request, DeserializeError> {
             Ok(Request::Handshake { mac })
         }
         consts::REQ_KIND_POST_RESULTS => {
-            let temperature = utils::deserialize_f32(&mut bytes)?;
-            let humidity = utils::next_byte(&mut bytes)?;
-            let air_pressure = utils::deserialize_optional(&mut bytes, utils::deserialize_u16)?;
+            let temperature = utils::deserialize_f32(bytes)?;
+            let humidity = utils::next_byte(bytes)?;
+            let air_pressure = utils::deserialize_optional(bytes, utils::deserialize_u16)?;
 
             Ok(Request::PostResults {
                 temperature,
@@ -178,9 +178,9 @@ pub fn deserialize_request(bytes: &[u8]) -> Result<Request, DeserializeError> {
             })
         }
         consts::REQ_KIND_POST_STATS => {
-            let battery = utils::deserialize_f32(&mut bytes)?;
-            let wifi_ssid = utils::deserialize_string(&mut bytes)?;
-            let wifi_rssi = utils::deserialize_i8(&mut bytes)?;
+            let battery = utils::deserialize_f32(bytes)?;
+            let wifi_ssid = utils::deserialize_string(bytes)?;
+            let wifi_rssi = utils::deserialize_i8(bytes)?;
 
             Ok(Request::PostStats {
                 battery,
@@ -189,20 +189,20 @@ pub fn deserialize_request(bytes: &[u8]) -> Result<Request, DeserializeError> {
             })
         }
         consts::REQ_KIND_SEND_NOTIFICATION => {
-            let content = utils::deserialize_string(&mut bytes)?;
+            let content = utils::deserialize_string(bytes)?;
             Ok(Request::SendNotification(content))
         }
         consts::REQ_KIND_GET_SETTINGS => Ok(Request::GetSettings),
         consts::REQ_KIND_UPDATE_CHECK => {
-            let version = utils::deserialize_version(&mut bytes)?;
+            let version = utils::deserialize_version(bytes)?;
             Ok(Request::UpdateCheck(version))
         }
         consts::REQ_KIND_NEXT_UPDATE_CHUNK => {
-            let amount = utils::deserialize_usize(&mut bytes)?;
+            let amount = utils::deserialize_usize(bytes)?;
             Ok(Request::NextUpdateChunk(amount))
         }
         consts::REQ_KIND_REPORT_FWU => {
-            let success = utils::deserialize_bool(&mut bytes)?;
+            let success = utils::deserialize_bool(bytes)?;
             Ok(Request::ReportFirmwareUpdate(success))
         }
         consts::REQ_KIND_BYE => Ok(Request::Bye),
@@ -211,11 +211,11 @@ pub fn deserialize_request(bytes: &[u8]) -> Result<Request, DeserializeError> {
 }
 
 /// Deserialize a response.
-pub fn deserialize_response(bytes: &[u8]) -> Result<Response, DeserializeError> {
-    let mut bytes = bytes.iter().copied();
-
+pub fn deserialize_response(
+    bytes: &mut impl Iterator<Item = u8>,
+) -> Result<Response, DeserializeError> {
     // get the response type
-    let res_type = next_byte(&mut bytes)?;
+    let res_type = next_byte(bytes)?;
 
     match res_type {
         consts::RES_KIND_PONG => Ok(Response::Pong),
@@ -227,11 +227,11 @@ pub fn deserialize_response(bytes: &[u8]) -> Result<Response, DeserializeError> 
         consts::RES_KIND_STALLING => Ok(Response::Stalling),
         consts::RES_KIND_FW_UTD => Ok(Response::FirmwareUpToDate),
         consts::RES_KIND_FW_UAVAIL => {
-            let version = utils::deserialize_version(&mut bytes)?;
+            let version = utils::deserialize_version(bytes)?;
             Ok(Response::UpdateAvailable(version))
         }
         consts::RES_KIND_FW_UPART => {
-            let blob = utils::deserialize_blob(&mut bytes)?;
+            let blob = utils::deserialize_blob(bytes)?;
             Ok(Response::UpdatePart(blob))
         }
         consts::RES_KIND_FW_UEND => Ok(Response::UpdateEnd),
@@ -240,11 +240,11 @@ pub fn deserialize_response(bytes: &[u8]) -> Result<Response, DeserializeError> 
                 return Ok(Response::Settings(None));
             }
 
-            let battery_ignore = utils::deserialize_bool(&mut bytes)?;
-            let ota = utils::deserialize_bool(&mut bytes)?;
-            let sleep_time = utils::deserialize_u16(&mut bytes)?;
-            let sbop = utils::deserialize_bool(&mut bytes)?;
-            let mute_notifications = utils::deserialize_bool(&mut bytes)?;
+            let battery_ignore = utils::deserialize_bool(bytes)?;
+            let ota = utils::deserialize_bool(bytes)?;
+            let sleep_time = utils::deserialize_u16(bytes)?;
+            let sbop = utils::deserialize_bool(bytes)?;
+            let mute_notifications = utils::deserialize_bool(bytes)?;
 
             Ok(Response::Settings(Some(NodeSettings {
                 battery_ignore,
@@ -260,19 +260,21 @@ pub fn deserialize_response(bytes: &[u8]) -> Result<Response, DeserializeError> 
 
 /// Deserialize a message.
 pub fn deserialize(bytes: &[u8]) -> Result<Message, DeserializeError> {
+    let mut bytes = bytes.iter().copied();
+
     // get the message ID
-    let msg_id = MsgId::from_ne_bytes(bytes[..4].try_into().unwrap());
+    let msg_id = utils::deserialize_u32(&mut bytes)?;
 
     // get the message type (req/res)
-    let msg_type = bytes[0];
+    let msg_type = utils::next_byte(&mut bytes)?;
 
     match msg_type {
         consts::MSG_KIND_REQUEST => {
-            let req = deserialize_request(&bytes[1..])?;
+            let req = deserialize_request(&mut bytes)?;
             Ok(Message::new_request(req, msg_id))
         }
         consts::MSG_KIND_RESPONSE => {
-            let res = deserialize_response(&bytes[1..])?;
+            let res = deserialize_response(&mut bytes)?;
             Ok(Message::new_response(res, msg_id))
         }
         _ => Err(DeserializeError::IllegalMessageType(msg_type)),
