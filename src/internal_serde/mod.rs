79a43fd62f8b6458b905ff915f465d5088247c46
br0kenpixel <23280129+br0kenpixel@users.noreply.github.com>
2026-03-31 19:51:55 +0200

    add enum for serializetion errors

--- src/internal_serde/mod.rs
+++ src/internal_serde/mod.rs
@@ -3,5 +3,8 @@
 mod deserializer;
 mod serializer;
 
+/// unfinished
+pub enum SerdeError {}
+
 pub use deserializer::{deserialize, DeserializeError};
 pub use serializer::{serialize, SerializeError};
